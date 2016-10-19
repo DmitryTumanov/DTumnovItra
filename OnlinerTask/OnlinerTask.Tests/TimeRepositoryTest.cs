9b0975014278b7796c4ad733d1f85aed4503ad83
d.tumanov <tumanov.97.dima@gmail.com>
2016-10-19 10:28:22 +0300

    Some new tests

---
+++ OnlinerTask/OnlinerTask.Tests/TimeRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlinerTask.Data.Repository;
+
+namespace OnlinerTask.Tests
+{
+    [TestClass]
+    public class TimeRepositoryTest
+    {
+        [TestMethod]
+        public void UpdateProductTest()
+        {
+            var msSqlTimeServiceRepository = new MsSqlTimeServiceRepository
+                (null, null);
+            var b = msSqlTimeServiceRepository.UpdateProduct
+                (null, null);
+            Assert.AreEqual(false, b);
+            Assert.IsNotNull(msSqlTimeServiceRepository);
+            Assert.IsNull(msSqlTimeServiceRepository.repository);
+        }
+
+        [TestMethod]
+        public void WriteUpdateToProductTest()
+        {
+            var msSqlTimeServiceRepository = new MsSqlTimeServiceRepository
+                (null, null);
+            var usersUpdateEmail = msSqlTimeServiceRepository.WriteUpdateToProduct
+                (null, default(TimeSpan));
+            Assert.IsNull(usersUpdateEmail);
+            Assert.IsNotNull(msSqlTimeServiceRepository);
+            Assert.IsNull(
+                msSqlTimeServiceRepository.repository);
+        }
+
+        [TestMethod]
+        public void WriteUpdateTest()
+        {
+            var msSqlTimeServiceRepository = new MsSqlTimeServiceRepository(null, null);
+            msSqlTimeServiceRepository.WriteUpdate(null, null);
+        }
+
+        [TestMethod]
+        public void GetAllProductsTest()
+        {
+            var msSqlRepository = new MsSqlRepository(null);
+            var msSqlTimeServiceRepository = new MsSqlTimeServiceRepository
+                (msSqlRepository, null);
+            msSqlTimeServiceRepository.GetAllProducts();
+        }
+    }
+}
