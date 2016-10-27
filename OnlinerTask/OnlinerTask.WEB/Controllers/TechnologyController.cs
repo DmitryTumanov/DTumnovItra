d6d8860ed49b4939d436ef31b2fa617d77a6a9f2
d.tumanov <tumanov.97.dima@gmail.com>
2016-10-27 16:43:56 +0300

    Admin role and changing technologies in runtime

---
+++ OnlinerTask/OnlinerTask.WEB/Controllers/TechnologyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OnlinerTask.Data.Requests;
+using OnlinerTask.Data.Resources;
+
+namespace OnlinerTask.WEB.Controllers
+{
+    public class TechnologyController : ApiController
+    {
+        public HttpResponseMessage Post(TechnologyRequest request)
+        {
+            Configurations.NotifyTechnology = request.Technology;
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
