afc8284240f5bbc9ef07c9de4ed47d1e2e5f640c
d.tumanov <tumanov.97.dima@gmail.com>
2016-11-29 13:43:50 +0300

    Replace code replications at notifacations publisher

--- OnlinerTask/OnlinerTask.BLL/Services/Notification/Implementations/NotifyJobService.cs
+++ OnlinerTask/OnlinerTask.BLL/Services/Notification/Implementations/NotifyJobService.cs
@@ -1,43 +1,41 @@
-﻿using OnlinerTask.Data.RedisManager.RedisServer.RedisRequests.Implementations;
+﻿using OnlinerTask.Data.MqConstituents;
+using OnlinerTask.Data.RedisManager.RedisServer.RedisRequests.Implementations;
 using ServiceStack.Messaging;
-using ServiceStack.Messaging.Redis;
-using ServiceStack.Redis;
 
 namespace OnlinerTask.BLL.Services.Notification.Implementations
 {
     public class NotifyJobService : INotification
     {
+        private readonly IMessageQueueClient queueClient;
+
+        public NotifyJobService(IMqConstituentsFactory constituentsFactory)
+        {
+            queueClient = constituentsFactory.CreateClient();
+        }
+
         public void AddProduct(dynamic name)
         {
-            CreateClient().Publish(new AddProductRequest() { Message = name.ToString()});
+            queueClient.Publish(new AddProductRequest() { Message = name.ToString() });
         }
 
         public void AddProductFromSearch(dynamic name)
         {
-            CreateClient().Publish(new AddProductRequest() { Message = name.ToString(), RedirectPath = "http://localhost:33399/Manage" });
+            queueClient.Publish(new AddProductRequest() { Message = name.ToString(), RedirectPath = "http://localhost:33399/Manage" });
         }
 
         public void ChangeSettings(dynamic time)
         {
-            CreateClient().Publish(new ChangeTimeRequest() {Message = time.ToString("t")});
+            queueClient.Publish(new ChangeTimeRequest() { Message = time.ToString("t") });
         }
 
         public void DeleteProduct(dynamic name)
         {
-            CreateClient().Publish(new RemoveProductRequest() { Message = name.ToString()});
+            queueClient.Publish(new RemoveProductRequest() { Message = name.ToString() });
         }
 
         public void DeleteProductFromSearch(dynamic name)
         {
-            CreateClient().Publish(new RemoveProductRequest() { Message = name.ToString(), RedirectPath = "http://localhost:33399/Manage" });
-        }
-
-        private IMessageQueueClient CreateClient()
-        {
-            var redisFactory = new PooledRedisClientManager("localhost:6379");
-            var mqServer = new RedisMqServer(redisFactory);
-            mqServer.Start();
-            return mqServer.CreateMessageQueueClient();
+            queueClient.Publish(new RemoveProductRequest() { Message = name.ToString(), RedirectPath = "http://localhost:33399/Manage" });
         }
     }
 }
