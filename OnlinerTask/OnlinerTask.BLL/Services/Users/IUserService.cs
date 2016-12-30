2ad75ec329fdc44ac0b114d11431180831f39af0
d.tumanov <tumanov.97.dima@gmail.com>
2016-12-30 16:33:20 +0300

    Add api controller and service for admin page

---
+++ OnlinerTask/OnlinerTask.BLL/Services/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OnlinerTask.Data.UserModels;
+
+namespace OnlinerTask.BLL.Services.Users
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAllUsers();
+        Task RemoveUser(string userName);
+        Task ChangeUserRole(UserModel user);
+    }
+}
